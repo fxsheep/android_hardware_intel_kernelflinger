cc3bac205ae2e5133c978565b41bddb2ba15373a
Andrew Boie <andrew.p.boie@intel.com>
2014-10-30 08:11:37 -0700

    git rid of stray label
    Change-Id: Ifdda30b9e9937dac4f9f4424cfe5a8132a8a43f6
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- kernelflinger.c
+++ kernelflinger.c
@@ -749,7 +749,6 @@ static VOID enter_fastboot_mode(UINT8 boot_state, VOID *keystore,
                 load_image(bootimage, boot_state);
         }
 
-exit:
         /* Allow plenty of time for the error to be visible before the
          * screen goes blank */
         pause(30);
